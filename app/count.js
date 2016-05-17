eceae5474f3c7d5aa33e57508b7a7998b7c2363a
Cornelius Toole <cornelius.toole@gmail.com>
2016-05-16 19:45:21 -0500

    solution for counter tests

--- app/count.js
+++ app/count.js
@@ -2,6 +2,18 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
-
+  	var counter_variable = start;
+  	console.log(counter_variable++);
+  	var intervalId = setInterval(function() {
+  		console.log(counter_variable++);
+  		if (counter_variable > end) {
+  			clearInterval(intervalId);
+  		}
+  	}, 100);
+  	return {
+  		cancel: function() {
+  			clearInterval(intervalId);
+  		}
+  	};
   }
 };
