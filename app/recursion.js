703a22a048b345366c3dcbf1b9219b9eb64f192a
Cornelius Toole <cornelius.toole@gmail.com>
2016-05-17 15:14:12 -0500

    incomplete: list needs more flattening

--- app/recursion.js
+++ app/recursion.js
@@ -2,7 +2,20 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
-
+  	console.log(data);
+  	var helper = function(d) {
+  		return d.map(function(item) {
+  			if (typeof item == "string") {
+  				return item
+  			}
+  			else if (item.files instanceof Array) {
+  				return helper(item.files);
+  			}
+  		})
+  	}
+  	var result = helper(data.files);
+  	var flattened = result.reduce(function(a,b){ return a.concat(b); }, []);
+  	return flattened;
   },
 
   permute: function(arr) {
