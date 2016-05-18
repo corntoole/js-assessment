a124024c3503a9c99ac18e25c2d34863883b58d2
Cornelius Toole <cornelius.toole@gmail.com>
2016-05-17 22:22:02 -0500

    solution for recursion assessments

--- app/recursion.js
+++ app/recursion.js
@@ -2,23 +2,21 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
-  	var helper = function(arr) {
-  		return arr.map (function(element) {
-  			if ( typeof element == "string") {
-  				return element;
-  			} else if ( element.files instanceof Array) {
-  				return helper(element.files);
+  	var fileNames = [];
+  	var helper = function(obj, dirs) {
+  		obj.files.forEach( function(element) {
+  			if (typeof element == "string") {
+  				if (dirName === undefined || dirs.indexOf(dirName) != -1) {
+  					fileNames.push(element);
+  				}
+  			} else if (element.files instanceof Array) {
+  				helper(element, dirs.concat(element.dir));
   			}
-  		})
-  	}
-  	return helper(data.files).reduce(
-  		function(a,b) {
-  			return a.concat(b);
-  		}, [])
-  	  .reduce(
-  		function(a,b) {
-  			return a.concat(b);
-  		}, []);
+  		}
+  		);
+  	};
+  	helper(data, [data.dir]);
+  	return fileNames;
   },
 
   permute: function(arr) {
